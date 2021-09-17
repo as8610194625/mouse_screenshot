2633dd7744e2ea0964444f3d0f467fd843cd5251
as8610194625 <as8610194625@gmail.com>
2021-09-17 16:35:56 +0800

    modify

--- laptop/tttttttttttttttkinter.py
+++ laptop/tttttttttttttttkinter.py
@@ -1,23 +1,19 @@
 import tkinter as tk
-from tkinter.constants import NO  # 使用Tkinter前需要先匯入
-from PIL import Image, ImageTk
+import keyboard
+ # 使用Tkinter前需要先匯入
 # 第1步，例項化object，建立視窗window
 class Winn(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.x = self.y = 0
-        # self.overrideredirect(True)
-        # 第2步，給視窗的視覺化起名字
         self.title('My Window')
-
-        # 第3步，設定視窗的大小(長 * 寬)
         self.geometry('{}x{}'.format(300,300))  # 這裡的乘是小x
-        # self.attributes("-alpha", 0.2)
-        self.attributes("-toolwindow", True)
-        # 第4步，在圖形介面上建立 500 * 200 大小的畫布並放置各種元素
+        # self.attributes("-toolwindow", True)
         button = tk.Button(self,text='start',command=self.win).pack()
         self.bind('<F1>',self.quit)
-    def win(self):
+        self.bind('<Control-c>',self.win)
+        # self.state("icon")
+    def win(self,event):
         self.top = tk.Toplevel()
         self.x = self.y = 0
         self.top.overrideredirect(True)
@@ -29,7 +25,7 @@ class Winn(tk.Tk):
         self.top.attributes("-alpha", 0.2)
         self.top.attributes("-toolwindow", True)
         # self.bind('<F3>',self.cv)
-
+        
     # def cv(self,event):
         
         self.canvas = tk.Canvas(self.top,width=self.winfo_screenwidth(), height=self.winfo_screenheight(),bg='grey')
@@ -39,15 +35,14 @@ class Winn(tk.Tk):
         self.rect = None
         start_x,start_y =None,None
 
-        self.canvas.bind("<F2>", self.cv_quit)
+        self.top.bind("<F2>", self.cv_quit)
         self.canvas.pack()
-        self.bind('<F3>',self.quit)
+        self.top.bind('<F3>',self.quit)
+
     def quit(self,event):
         # global self
         self.destroy()
     # 第6步，觸發函式，用來一定指定圖形
-    def moveit(self):
-        self.canvas.move(self.rect, 2, 2) # 移動正方形rect（也可以改成其他圖形名字用以移動一起圖形、元素），按每次（x=2, y=2）步長進行移動
     def on_button_press(self, event):
         # save mouse drag start position
         self.start_x = event.x
@@ -65,5 +60,7 @@ class Winn(tk.Tk):
     # 第7步，主視窗迴圈顯示
 
 if __name__ == '__main__':
+    # window = Winn()
     window = Winn()
     window.mainloop()
+
